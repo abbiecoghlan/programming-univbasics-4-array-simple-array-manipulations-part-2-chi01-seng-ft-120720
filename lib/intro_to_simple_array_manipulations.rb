0cd53966f6f92191fb8bd2adf9fc1b68c105c0f5
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-02 22:48:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,11 +10,12 @@ def using_uniq(array)
   array.uniq
 end 
 
-def using_flatten
-  
+def using_flatten(array)
+  array.flatten
 end 
 
-def using_delete
+def using_delete(array, string)
+  array.
 end 
 
 def using_delete_at
